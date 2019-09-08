e19ebd0bf19bc3ca17d99a2babec5f22b29498c7
sergey <mrqiwi@ya.ru>
2019-09-08 21:06:01 +0300

    add async tcp server examples

---
+++ net/server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(('localhost', 5000))
+server_socket.listen()
+
+while True:
+
+    client_socket, addr = server_socket.accept()
+    print('Connection from', addr)
+
+    while True:
+        request = client_socket.recv(4096)
+
+        if not request:
+            break
+        else:
+            response = 'Hello world\n'.encode() # кодируем в bytes    
+            client_socket.send(response)
+
+    client_socket.close()
